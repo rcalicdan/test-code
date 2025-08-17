4e97758d45dddbd3457a3bb5b0b9f0e6cb21c312
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2025-08-18 01:32:33 +0800

    testss

--- asarinos.php
+++ asarinos.php
@@ -19,17 +19,17 @@ define('ASARINOS_VERSION', '1.3');
 
 require_once ASARINOS_PLUGIN_DIR . 'vendor/autoload.php';
 
-require_once ASARINOS_PLUGIN_DIR . 'includes/class-asarinos-core.php';
-require_once ASARINOS_PLUGIN_DIR . 'includes/class-asarinos-media.php';
-require_once ASARINOS_PLUGIN_DIR . 'includes/class-asarinos-property-manager.php';
-require_once ASARINOS_PLUGIN_DIR . 'includes/class-asarinos-api-client.php';
-require_once ASARINOS_PLUGIN_DIR . 'includes/class-asarinos-admin.php';
-require_once ASARINOS_PLUGIN_DIR . 'includes/class-asarinos-frontend.php';
-require_once ASARINOS_PLUGIN_DIR . 'includes/class-asarinos-cron.php';
-require_once ASARINOS_PLUGIN_DIR . 'includes/class-asarinos-shortcodes.php';
+require_once ASARINOS_PLUGIN_DIR . 'includes/asarinos/class-asarinos-core.php';
+require_once ASARINOS_PLUGIN_DIR . 'includes/asarinos/class-asarinos-media.php';
+require_once ASARINOS_PLUGIN_DIR . 'includes/asarinos/class-asarinos-property-manager.php';
+require_once ASARINOS_PLUGIN_DIR . 'includes/asarinos/class-asarinos-api-client.php';
+require_once ASARINOS_PLUGIN_DIR . 'includes/asarinos/class-asarinos-admin.php';
+require_once ASARINOS_PLUGIN_DIR . 'includes/asarinos/class-asarinos-frontend.php';
+require_once ASARINOS_PLUGIN_DIR . 'includes/asarinos/class-asarinos-cron.php';
+require_once ASARINOS_PLUGIN_DIR . 'includes/asarinos/class-asarinos-shortcodes.php';
 
 // Include the refactored shortcode
-require_once ASARINOS_PLUGIN_DIR . 'main-page-shortcode.php';
+require_once ASARINOS_PLUGIN_DIR . 'main_page_shortcode.php';
 
 // Initialize the plugin
 function asarinos_init()
